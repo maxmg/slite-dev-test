eab6183476301f4e1e1e558fe2ec9b6412107b7c
MaximeBonobots <maxime.mangin@gmail.com>
2018-06-06 09:31:56 +0200

    commands ADD: server setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+const net = require('net')
+
+const HOST = 'localhost'
+const PORT = 1337
+
+const server = net.createServer((socket) => {
+  socket.on('error', (err) => console.log(err))
+  socket.on('data', (data) => {
+    socket.write('200\r\n')
+  })
+})
+
+server.listen(PORT, HOST)
